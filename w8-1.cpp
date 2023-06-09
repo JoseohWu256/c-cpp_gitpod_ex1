5a724f436e8ae25982bbce2c9e065db0f07bcb50
JosephWu256 <131453827+JoseohWu256@users.noreply.github.com>
2023-06-09 19:59:02 +0800

    Update w8-1.cpp

--- w8-1.cpp
+++ w8-1.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 #if 0
 int f(int n){
-    if(n<1){
+    if(n<=1){
         return 0;
     }else{
-        return n+f(n-1);
+        return n*f(n-1);
     }
 }
 #else
 int f(int n){
-    int val=0;
+    int val=1;
     int i;
-    for(i=1;i<=n;i++){
-        val+=i;
+    for(i=2;i<=n;i++){
+        val*=i;
     }
     return val;
 }
 #endif
 int main(){
-    cout<<"sum = "<<f(5)<<endl;
+    cout<<"5! = "<<f(5)<<endl;
     return 0;
 }
