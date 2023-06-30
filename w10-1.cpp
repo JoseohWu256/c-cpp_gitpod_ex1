084e4975e6fe14846ce685ff3315d33b867da471
JosephWu256 <131453827+JoseohWu256@users.noreply.github.com>
2023-06-30 19:48:45 +0800

    Create w10-1.cpp

---
+++ w10-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void display(int age1,int age2){
+    printf("%d\n",age1);
+    printf("%d\n",age2);
+}
+int main() {
+    int ageArr[]={2,8,4,12};
+    display(ageArr[1],ageArr[2]);
+}
